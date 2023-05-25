c829b5ed68a8c01424f63d2006a04a33c1eb2667
lais.lopes <laisjkl@hotmail.com>
2023-05-25 11:51:22 -0300

    Add Socket.io into the project.

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import url from "url";
+import path from "path";
+import http from "http";
+import { Server} from "socket.io";
+
+const app = express();
+const port = process.env.port || 3000;
+
+const currentPath = url.fileURLToPath(import.meta.url);
+const publicDirectory = path.join(currentPath, "../..", "public");
+app.use(express.static(publicDirectory));
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
+
+const io = new Server(httpServer);
+
+io.on("connection", () =>{
+    console.log("One client has been connected!")
+});
